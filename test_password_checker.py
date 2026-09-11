00b55108b4d6f52e7855a50e2e118b1e1a0687f4
JohnPaulSelmatec <j.paul@selmatec.de>
2026-09-11 10:57:53 +0200

    Add test cases for password validation
    nice

--- test_password_checker.py
+++ test_password_checker.py
@@ -1,1 +1,9 @@
+is_valid_password(Abcde123)
+is_valid_password(abcde123)
+is_valid_password(Abcdefgh)
+is_valid_password(123aV)
+is_valid_password(abcdefgh)
+is_valid_password(abch3)
+is_valid_password(abcH)
+is_valid_password(ABD)
 
