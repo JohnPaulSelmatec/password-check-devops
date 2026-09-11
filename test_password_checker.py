e571ff553838c3ff95ed5f99ad05763dfdef133c
JohnPaulSelmatec <j.paul@selmatec.de>
2026-09-11 11:21:41 +0200

    Add test for is_valid_password function
    Added a test function to validate password criteria.

--- test_password_checker.py
+++ test_password_checker.py
@@ -1,12 +1,15 @@
 from password_checker import is_valid_password
 
 
-is_valid_password("Abcde123")
-is_valid_password("abcde123")
-is_valid_password("Abcdefgh")
-is_valid_password("123aV")
-is_valid_password("abcdefgh")
-is_valid_password("abch3")
-is_valid_password("abcH")
-is_valid_password("ABD")
+def test1():
+  assert is_valid_password("Abcde123") == True
+
+#is_valid_password("Abcde123")
+#is_valid_password("abcde123")
+#is_valid_password("Abcdefgh")
+#is_valid_password("123aV")
+#is_valid_password("abcdefgh")
+#is_valid_password("abch3")
+#is_valid_password("abcH")
+#is_valid_password("ABD")
 
